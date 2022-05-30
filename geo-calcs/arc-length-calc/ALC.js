1a8db0fe23260979eea99de2bda2359a28778274
Pondering Ape <59585655+LukeLeary@users.noreply.github.com>
2022-05-30 12:30:56 -0400

    Add files via upload

---
+++ geo-calcs/arc-length-calc/ALC.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef, no-unused-vars */
+let inp;
+let inp2;
+
+function setup() {
+  createCanvas(600, 180 * 3);
+  inp = createInput("0");
+  inp.position(50, 520);
+  inp.size(200);
+  inp2 = createInput("0");
+  inp2.position(290, 520);
+  inp2.size(200);
+}
+
+function draw() {
+  //some geometry
+  let arclength = 2 * Math.PI * inp.value() * (inp2.value() / 360);
+
+  //body
+  push();
+  background(0);
+  fill(255);
+  rect(0, 0, 600, 180 * 3, 20);
+  pop();
+
+  //formating
+  push();
+  scale(3);
+  translate(0, 20);
+  textStyle(BOLD);
+  //text
+  fill(0);
+  textSize(40);
+  text(arclength, 20, 125 - 40);
+  textSize(20);
+  text("Arc Length", 20, 125 - 75);
+  textSize(8);
+  text("Radius", 20, 137);
+  text("Central Angle", 100, 137);
+  pop();
+}
---
+++ geo-calcs/hypotenuse-calc/HC.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef, no-unused-vars */
+let inp;
+let inp2;
+
+function setup() {
+  createCanvas(600, 180 * 3);
+  inp = createInput("0");
+  inp.position(50, 520);
+  inp.size(200);
+  inp2 = createInput("0");
+  inp2.position(290, 520);
+  inp2.size(200);
+}
+
+function draw() {
+  //some geometry
+  let hypotenuse = Math.sqrt(
+    Math.pow(inp.value(), 2) + Math.pow(inp2.value(), 2)
+  );
+
+  //body
+  push();
+  background(0);
+  fill(255);
+  rect(0, 0, 600, 180 * 3, 20);
+  pop();
+
+  //formating
+  push();
+  scale(3);
+  translate(0, 20);
+  textStyle(BOLD);
+  //text
+  fill(0);
+  textSize(40);
+  text(hypotenuse, 20, 125 - 40);
+  textSize(20);
+  text("Hypotenuse", 20, 125 - 75);
+  textSize(8);
+  text("Leg A", 20, 137);
+  text("Leg B", 100, 137);
+  pop();
+}
